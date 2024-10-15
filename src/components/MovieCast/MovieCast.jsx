be80d1b01bfc5cdd318f8a039638da8c633b6fb7
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-10-15 19:36:45 +0200

    add function for the cast of the movie.

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -2,53 +2,55 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 
-
   const MovieCast = () => {
-  const { movieId } = useParams(); // отримуємо movieId з URL
-  const [actor, setActors] = useState([]); //ініціалізуєсмо як null //Початковий стан для movie має бути об'єктом (а не масивом), оскільки отриму деталі одного фільму.
+    const { movieId } = useParams(); // отримуємо movieId з URL
+    const [actors, setActors] = useState([]); //ініціалізуєсмо як null //Початковий стан для movie має бути масивом (а не обєктом), оскільки отриму список акторів фільму.
 
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTcwYWFlNjYwODI1YTU5ZGVmNjBlMWYwM2MwOGIyZCIsIm5iZiI6MTcyODgzOTExMi4yMjc5NTcsInN1YiI6IjY3MGFjYmQ5ZjU4YTkyMDZhYTQwOTc4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.84dq98WVNAZPFKBnzzHxLrqf0UO02tBIbsj3mVjISbA",
-    },
-  };
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTcwYWFlNjYwODI1YTU5ZGVmNjBlMWYwM2MwOGIyZCIsIm5iZiI6MTcyODgzOTExMi4yMjc5NTcsInN1YiI6IjY3MGFjYmQ5ZjU4YTkyMDZhYTQwOTc4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.84dq98WVNAZPFKBnzzHxLrqf0UO02tBIbsj3mVjISbA",
+      },
+    };
 
-  const fetchMovieActors = async () => {
-    try {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/${movieId}?/credits?language=en-US`,
-        options
-      );
-      const movieActors = await response.json();
-      console.log(movieActors);
-      setActors(movieActors);
-    } catch (error) {
-      console.error("Error fetching the movie actors:", error);
+    const fetchMovieActors = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`,
+          options
+        );
+        const movieActors = await response.json();
+        //      console.log(movieActors);
+        setActors(movieActors.cast);
+      } catch (error) {
+        console.error("Error fetching the movie actors:", error);
+      }
+    };
+
+    useEffect(() => {
+      if (movieId) {
+        fetchMovieActors(); //виклик функції fetchTrending тільки під час початкового рендерингу програми
+      }
+    }, [movieId]);
+
+    if (!actors && setActors.length === 0) {
+      return <div>Loading...</div>; // Відображати стан завантаження, якщо дані фільму ще не доступні
     }
-  };
 
-  useEffect(() => {
-    if (movieId) {
-      fetchMovieActors(); //виклик функції fetchTrending тільки під час початкового рендерингу програми
-    }
-  }, [movieId]);
-    
-  if (!actor) {
-    return <div>Loading...</div>; // Відображати стан завантаження, якщо дані фільму ще не доступні
-  }
-
-  return (
-    <div>
-      <ul>
-        <li>
-          {}
-        </li>
-</ul>
-    </div>
-  )
-}; 
+    return (
+      <div>
+        <h3>Cast</h3>
+        <ul>
+          {actors.map((actor) => (
+            <li key={actor.id}>
+              {actor.name} as {actor.character} {/* Ім'я актора і його роль */}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }; 
     
     export default MovieCast;
